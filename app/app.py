ecc6f827fa23307d4a3b76001bf1760e1e19d37f
Pritalee Kadam <pritaleekadam000@gmail.com>
2023-04-17 21:26:13 -0600

    remove secrets

--- app/app.py
+++ app/app.py
@@ -9,10 +9,6 @@ import string
 import json
 
 
-# CLIENT_ID = '6dfeb3b208d644cdb26620d00611eacd'
-# CLIENT_SECRET = 'd1a321a4aa1f4d0e84b631581c97356b'
-# SECRET_KEY = 'secret'
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.secret_key = SECRET_KEY
